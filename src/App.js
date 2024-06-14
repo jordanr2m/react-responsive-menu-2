588ce546f5d554a0cc075702bc89960b1cb80fa6
Jordan J <jordanr2m@gmail.com>
2024-06-14 16:37:15 -0700

    Fixed color of header.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <header className="header">
       <Navbar />
       <Header/>
-    </div>
+    </header>
   );
 }
 
